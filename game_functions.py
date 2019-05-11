94647d6cdf2d2e541a17b7fa55599db2143e84f7
Hitesh Kandala <45922320+v1an1@users.noreply.github.com>
2019-05-11 14:31:23 +0530

    Add files via upload

--- game_functions.py
+++ game_functions.py
@@ -2,11 +2,30 @@ import sys
 
 import pygame
 
-def check_events():
+def check_keydown_events(event, ship):
+	""" Respond to keypresses"""
+	if event.type == pygame.K_RIGHT:
+		ship.moving_right = True
+	elif event.type == pygame.K_LEFT:
+		ship.moving_left = True
+
+def check_keyup_events(event, ship):
+	if event.key == pygame.K_RIGHT:
+		ship.moving_right = False
+	elif event.key == pygame.K_LEFT:
+		ship.moving_left = False
+
+def check_events(ship):
 	""" Respond to keyresponses and mouse events."""
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			sys.exit()
+
+		elif event.type == pygame.KEYDOWN:
+			check_keydown_events(event, ship)
+
+		elif event.type == pygame.KEYUP:
+			check_keyup_events(event, ship)
 
 def update_screen(ai_settings, screen, ship):
 	""" Update images on the screen and flip to the new screen."""
